20ee3522b1a16972140d2f226a03e4877ac2c261
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-04-14 16:53:29 +0300

    Add ability to add new animal

--- FarmWebAppClient/src/app/animals/animals-list/animals-list.component.ts
+++ FarmWebAppClient/src/app/animals/animals-list/animals-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, WritableSignal, signal } from '@angular/core';
 import {
   BehaviorSubject,
   Observable,
@@ -11,15 +11,17 @@ import {
 import { AnimalComponent } from 'animals/animal/animal.component';
 import { Animal } from 'models';
 import { AnimalsApiService } from 'services/animals-api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-animals-list',
   standalone: true,
-  imports: [CommonModule, AnimalComponent],
+  imports: [CommonModule, FormsModule, AnimalComponent],
   templateUrl: './animals-list.component.html',
   styleUrl: './animals-list.component.scss',
 })
 export class AnimalsListComponent implements OnInit {
+  newAnimalName: WritableSignal<string> = signal('');
   animals$!: Observable<Animal[]>;
 
   private reloadSubject = new BehaviorSubject(null);
@@ -40,9 +42,32 @@ export class AnimalsListComponent implements OnInit {
     );
   }
 
+  async addAnimal(): Promise<void> {
+    if (!this.newAnimalName()) {
+      alert('Animal name should be set');
+      return;
+    }
+
+    try {
+      await firstValueFrom(
+        this.animalsApi.create({ name: this.newAnimalName() })
+      );
+
+      this.newAnimalName.set('');
+
+      this.reloadData();
+    } catch {
+      alert('An error occurred during the API call. Please try again later.');
+    }
+  }
+
   async deleteAnimal(animal: Animal): Promise<void> {
-    await firstValueFrom(this.animalsApi.delete(animal.name));
-    this.reloadData();
+    try {
+      await firstValueFrom(this.animalsApi.delete(animal.name));
+      this.reloadData();
+    } catch {
+      alert('An error occurred during the API call. Please try again later.');
+    }
   }
 
   private reloadData(): void {
