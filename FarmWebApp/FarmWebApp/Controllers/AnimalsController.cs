08e2d668c52b9ddf449cc52e790d0c6d7176f030
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-04-14 14:50:34 +0300

    Add empty animals controller

---
+++ FarmWebApp/FarmWebApp/Controllers/AnimalsController.cs
@@ -0,0 +1,31 @@
+﻿namespace FarmWebApp.Controllers;
+
+using FarmWebApp.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("animals")]
+public class AnimalsController : ControllerBase
+{
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        return this.Ok(Array.Empty<AnimalDto>());
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<IActionResult> Create([FromBody] AddAnimalRequest request, CancellationToken cancellationToken)
+    {
+        return this.Ok(null);
+    }
+
+    [HttpDelete]
+    [Route("{name}")]
+    public async Task<IActionResult> Delete(string name, CancellationToken cancellationToken)
+    {
+        return this.Ok();
+    }
+}
