3d45037f8b1c5c510eab80f0660aa9cc77481ba8
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-04-14 15:11:22 +0300

    Implement animals controller

--- FarmWebApp/FarmWebApp/Controllers/AnimalsController.cs
+++ FarmWebApp/FarmWebApp/Controllers/AnimalsController.cs
@@ -1,5 +1,7 @@
 ﻿namespace FarmWebApp.Controllers;
 
+using FarmWebApp.Data;
+using FarmWebApp.Exceptions;
 using FarmWebApp.Models;
 
 using Microsoft.AspNetCore.Mvc;
@@ -8,24 +10,64 @@ using Microsoft.AspNetCore.Mvc;
 [Route("animals")]
 public class AnimalsController : ControllerBase
 {
+    private readonly IAnimalsStorage animalsStorage;
+
+    public AnimalsController(IAnimalsStorage animalsStorage)
+    {
+        this.animalsStorage = animalsStorage;
+    }
+
     [HttpGet]
     [Route("")]
     public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
     {
-        return this.Ok(Array.Empty<AnimalDto>());
+        var animals = await this.animalsStorage.GetAll(cancellationToken);
+        var animalsDtos = animals.Select(ToDto).ToArray();
+        return this.Ok(animalsDtos);
     }
 
     [HttpPost]
     [Route("")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<IActionResult> Create([FromBody] AddAnimalRequest request, CancellationToken cancellationToken)
     {
-        return this.Ok(null);
+        try
+        {
+            await this.animalsStorage.Add(request.Name!, cancellationToken);
+            return this.Created();
+        }
+        catch (ArgumentException ex)
+        {
+            return this.BadRequest(ex.Message);
+        }
+        catch (DuplicateEntityException ex)
+        {
+            return this.Conflict(ex.Message);
+        }
     }
 
     [HttpDelete]
     [Route("{name}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Delete(string name, CancellationToken cancellationToken)
     {
-        return this.Ok();
+        try
+        {
+            await this.animalsStorage.Remove(name, cancellationToken);
+            return this.Ok();
+        }
+        catch (EntityNotFoundException ex)
+        {
+            return this.NotFound(ex.Message);
+        }
+    }
+
+    private static AnimalDto ToDto(Animal entity)
+    {
+        return new AnimalDto
+        {
+            Name = entity.Name
+        };
     }
 }
